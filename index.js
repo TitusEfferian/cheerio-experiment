4403c31b993191510ae6b4dcbbc0c533827e0d1f
titusefferian <kiritosato@gmail.com>
2018-11-25 19:59:34 +0700

    horrible schedule scrap

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ import express from 'express'
 import cheerio from 'cheerio'
 import rp from 'request-promise'
 import profileParser from './profile'
+import axios from 'axios'
 const app = express()
-const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+const url = 'https://www.tokopedia.com/';
 
 app.get('/scrap', function (req, res) {
-  const wikiUrl = []
-  rp(url)
-  .then((html)=>{
-    for(let a = 0 ; a<45;a++){
-      wikiUrl.push(cheerio('big > a',html)[a].attribs.href)
-    }
-    return Promise.all(
-      wikiUrl.map(x=>profileParser('https://en.wikipedia.org'+x))
-    )
-  })
-  .then((president)=>{
-    res.send({success:true,data:president})
-  })
-  .catch((err)=>{
-    console.log(err)
-  })
+  axios.get('https://horriblesubs.info/')
+    .then((response) => {
+      if (response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const show = []
+        $('table.schedule-table > tbody > tr').each(function () {
+          show.push({
+            title: $(this).find('a').text(),
+            time: $(this).find('.schedule-widget-time').text()
+          })
+        })
+        res.send(show)
+      }
+    })
 })
 
 app.listen('8081')
